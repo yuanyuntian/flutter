498552c2a3244de00d633f44234420a087db0dec
yuanyuntian <yuanf1990@163.com>
2022-07-27 18:56:20 +0800

    card

--- mycard/lib/main.dart
+++ mycard/lib/main.dart
@@ -24,7 +24,86 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold()
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/angela.jpg')
+                ),
+                const Text('Angle Yu',style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                )),
+                const Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white
+                  ),
+                ),
+                 Card(
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile (
+                    leading:  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                         Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        )
+                      ],
+                    ),
+                    title: const Text(
+                      '+44 123 456 789',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    subtitle: const Text(
+                      '+44 123 456 789',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  )),
+                  const Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'angela@email.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal,
+                            fontFamily: 'Source Sans Pro'),
+                      ),
+                    ))
+              ],
+        )),
+      )
     );
   }
 }
